3e851ee12d0c3f6f9e9523a4dcef9626aeeecf80
valentinlisci <valentin.lisci@sourcesense.com>
2025-07-07 09:21:42 +0200

    versione corretta0.1

--- script_ODD:EVEN.sh
+++ script_ODD:EVEN.sh
@@ -1,19 +1,19 @@
 #!/bin/bash
-#script ODD/EVEN
+# Script ODD/EVEN
 echo "Benvenuto!"
-read -p "Inserisci un numero! non sono accettate lettere o parole: " numero
-if [[ "$numero" =~ ^-?[0-9]*\.?[0-9]+$ ]]; then
-    echo "Hai inserito un numero!"
+read -p "Inserisci un numero intero positivo: " numero
+if [[ "$numero" =~ ^[1-9][0-9]*$ ]]; then
+    echo "Hai inserito un numero valido!"
 else
-    echo "Per favore, inserisci un numero valido."
+    echo "Per favore, inserisci un numero intero positivo."
     exit 1
 fi
-if (( numero % 2 == 0 )); then
-    echo "Il numero $numero è pari."
-else
-    echo "Il numero $numero è dispari."
-fi
+
 for ((i=1; i<=numero; i++)); do
-    echo "$i"
+    if (( i % 2 == 0 )); then
+        echo "$i è pari."
+    else
+        echo "$i è dispari."
+    fi
 done
 # :)
